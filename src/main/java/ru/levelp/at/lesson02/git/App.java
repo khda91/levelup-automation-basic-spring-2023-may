e765333e7731944f3c223b193c8f457bc7b22202
Khodakovsky Dmitry <khda91@gmail.com>
2023-05-18 19:52:12 +0300

    Added additional Calculator add example

--- src/main/java/ru/levelp/at/lesson02/git/App.java
+++ src/main/java/ru/levelp/at/lesson02/git/App.java
@@ -6,5 +6,6 @@ public class App {
         Calculator calculator = new CalculatorImpl();
 
         System.out.println("2.4 + 2.8 = " + calculator.add(2.4, 2.8));
+        System.out.println("5.3 + 0.7 = " + calculator.add(5.3, 0.7));
     }
 }
