c2722d655181485a9b922b5c14aa7f17a112ade9
Khodakovskiy Dmitriy <khda91@gmail.com>
2023-05-18 21:08:41 +0300

    add add example

--- src/main/java/ru/levelp/at/lesson02/git/App.java
+++ src/main/java/ru/levelp/at/lesson02/git/App.java
@@ -7,7 +7,7 @@ public class App {
 
         System.out.println("2.4 + 2.8 = " + calculator.add(2.4, 2.8));
         System.out.println("5.3 + 0.7 = " + calculator.add(5.3, 0.7));
-
+        System.out.println("5.3 + 0.7 = " + calculator.add(-0.7, 0.7));
 
         System.out.println("2.4 - 2.8 = " + calculator.subtract(2.4, 2.8));
         System.out.println("5.3 - 0.7 = " + calculator.subtract(5.3, 0.7));
