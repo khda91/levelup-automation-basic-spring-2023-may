16b2b2973ece09c3b2ab12f3af446d4af7dbca43
Khodakovskiy Dmitriy <khda91@gmail.com>
2023-05-18 21:14:44 +0300

    add divide example fix errors

--- src/main/java/ru/levelp/at/lesson02/git/App.java
+++ src/main/java/ru/levelp/at/lesson02/git/App.java
@@ -7,17 +7,18 @@ public class App {
 
         System.out.println("2.4 + 2.8 = " + calculator.add(2.4, 2.8));
         System.out.println("5.3 + 0.7 = " + calculator.add(5.3, 0.7));
-        System.out.println("5.3 + 0.7 = " + calculator.add(-0.7, 0.7));
+        System.out.println("-0.7 + 0.7 = " + calculator.add(-0.7, 0.7));
 
         System.out.println("2.4 - 2.8 = " + calculator.subtract(2.4, 2.8));
         System.out.println("5.3 - 0.7 = " + calculator.subtract(5.3, 0.7));
-        System.out.println("5.3 - 0.7 = " + calculator.subtract(-0.9, 0.7));
+        System.out.println("-0.9 - 0.7 = " + calculator.subtract(-0.9, 0.7));
 
         System.out.println("2.4 * 2.8 = " + calculator.multiply(2.4, 2.8));
         System.out.println("5.3 * 0.7 = " + calculator.multiply(5.3, 0.7));
-        System.out.println("5.3 * 0.7 = " + calculator.multiply(0.3, 0.7));
+        System.out.println("0.3 * 0.7 = " + calculator.multiply(0.3, 0.7));
 
         System.out.println("2.4 / 2.8 = " + calculator.divide(2.4, 2.8));
         System.out.println("5.3 / 0.7 = " + calculator.divide(5.3, 0.7));
+        System.out.println("0.0 / 0.7 = " + calculator.divide(0.0, 0.7));
     }
 }
